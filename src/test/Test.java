238c012cf9c7cadcda282dfbf7f6f1717f2411c1
MrYangHui <17302010051@fudan.edu.cn>
2020-12-30 13:54:16 +0800

    init

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author: YXH
+ * @date: 2020/12/30
+ * @time: 13:48
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("1");
+    }
+}
