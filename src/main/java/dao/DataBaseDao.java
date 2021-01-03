1b839a79df05f9ac0a0245b2e70d290fdfc594ca
Chukaidong <17302010007@fudan.edu.cn>
2021-01-03 17:11:18 +0800

    add property

--- src/main/java/dao/DataBaseDao.java
+++ src/main/java/dao/DataBaseDao.java
@@ -3,40 +3,14 @@ package dao;
 import bean.Product;
 import bean.executor.Expert;
 import bean.executor.Market;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 /**
  * @author: YXH
  * @date: 2021/1/1
  * @time: 14:16
  */
+
 public class DataBaseDao {
-
-    private static final String JDBC_URL = "jdbc:h2:file: ../../db";
-    private static final String USER = "db";
-    private static final String PASSWORD = "db";
-    private static final String DRIVER_CLASS="org.h2.Driver";
-
-    public static void main(String[] args) throws Exception {
-        // 加载H2数据库驱动
-        Class.forName(DRIVER_CLASS);
-        // 根据连接URL，用户名，密码获取数据库连接
-        Connection conn = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
-        Statement stmt = conn.createStatement();
-
-//        ResultSet rs = stmt.executeQuery("SELECT * FROM EXPERT");
-//        while (rs.next()){
-//            System.out.println(rs.getInt(1) + "/t" +    rs.getString(2));
-//        }
-
-        //释放资源
-        stmt.close();
-        conn.close();
-    }
-
     public Expert selectExpertByName(String name){
         Expert expert = new Expert();
         expert.setName(name);
